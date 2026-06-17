b5422a169adc0091f3cd74eaabf38ab8a7572254
abdellahi <abdellahi123k-gif>
2026-06-17 09:17:03 +0000

    first commit

--- frontend/frontend-react/src/pages/Dashboard.jsx
+++ frontend/frontend-react/src/pages/Dashboard.jsx
@@ -22,6 +22,7 @@ const icons = {
   enAttente: <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>,
   confirme: <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>,
   termine: <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16"><path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/><path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/></svg>,
+  annule: <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>,
 };
 
 export default function Dashboard() {
@@ -64,17 +65,20 @@ export default function Dashboard() {
       <ErrorAlert message={error} onClose={() => setError(null)} />
 
       <div className="row g-3 mb-4">
-        <div className="col-12 col-sm-6 col-xl-3">
+        <div className="col-12 col-sm-6 col-xl-2">
           <StatCard title={t('dashboard.total')} value={stats.total} color="primary" icon={icons.total} />
         </div>
-        <div className="col-12 col-sm-6 col-xl-3">
+        <div className="col-12 col-sm-6 col-xl-2">
           <StatCard title={t('dashboard.enAttente')} value={stats.enAttente} color="warning" icon={icons.enAttente} />
         </div>
-        <div className="col-12 col-sm-6 col-xl-3">
+        <div className="col-12 col-sm-6 col-xl-2">
           <StatCard title={t('dashboard.confirme')} value={stats.confirme} color="success" icon={icons.confirme} />
         </div>
-        <div className="col-12 col-sm-6 col-xl-3">
+        <div className="col-12 col-sm-6 col-xl-2">
           <StatCard title={t('dashboard.termine')} value={stats.termine} color="info" icon={icons.termine} />
+        </div>
+        <div className="col-12 col-sm-6 col-xl-2">
+          <StatCard title={t('dashboard.annule')} value={stats.annule} color="danger" icon={icons.annule} />
         </div>
       </div>
 
